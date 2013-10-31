d42d4aba7a9048aad62cc6d4173db8222810eebf
Fred Myers <fred@fred-myers.com>
2013-10-31 13:36:20 -0400

    Setup Login and Sign pages in storyboard.

---
+++ FMInboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMInboxViewController.h
+//  Ribbit
+//
+//  Created by Fredrick Myers on 10/31/13.
+//  Copyright (c) 2013 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMInboxViewController : UITableViewController
+
+@end
