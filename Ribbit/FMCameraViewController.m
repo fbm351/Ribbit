1f3004e54633c2dba76b13dbcb0ea83b4b8606cd
Fred Myers <fred@fred-myers.com>
2013-11-07 16:46:54 -0500

    Fixed Refresh friends issue.

--- Ribbit/FMCameraViewController.m
+++ Ribbit/FMCameraViewController.m
@@ -19,13 +19,15 @@
 {
     [super viewDidLoad];
     
-    self.friendsRelation = [[PFUser currentUser] objectForKey:@"friendsRelation"];
     self.recipients = [[NSMutableArray alloc] init];
     
 }
 
 - (void) viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    
+    self.friendsRelation = [[PFUser currentUser] objectForKey:@"friendsRelation"];
+
     
     PFQuery *query = [self.friendsRelation query];
     [query orderByAscending:@"username"];
